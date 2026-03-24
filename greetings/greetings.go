de1ea3d3dfe47839001cf1fdb1e50b6db2e04af7
mallsR <2471385107@qq.com>
2026-03-24 10:56:19 +0800

    更新问候功能，添加错误处理并修改日志设置

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello 为指定的人返回问候语.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// 如果没有给出名字，返回一个带有消息的错误.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// 返回在消息中嵌入名称的问候语.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name) // Sprintf会将格式化的问候文本返回给调用者.
-	return message
+	return message, nil                              // 这里的nil表示没有错误发生.
 }
--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"go_learn/greetings"
 
@@ -10,7 +11,20 @@ import (
 
 func main() {
 	fmt.Println(quote.Go())
+	// 设置预定义Logger的属性，包括
+	// 日志条目前缀和禁用打印的标志
+	//  时间、源文件和行号.
+	log.SetPrefix("greetings: ") // 这里的前缀"greetings: "将被添加到每条日志消息的开头.
+	log.SetFlags(0)              // 这里的0表示禁用所有默认的日志标志.
 
-	message := greetings.Hello("5000")
+	message, err := greetings.Hello("")
+	// 如果返回错误，则将其打印到控制台并
+	// 退出程序.
+	if err != nil {
+		log.Fatal(err) // log.Fatal()会打印错误消息并调用os.Exit(1)，因此程序会在这里终止.
+	}
+
+	// 如果没有返回错误，则打印返回的消息
+	// 到控制台.
 	fmt.Println(message)
 }
